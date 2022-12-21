aea2005e724e478490a37df2f4dc90f714beb486
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2022-12-21 12:15:46 -0700

    Make hero and nav responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import '../css/home.css'
 import { MDBBtn, MDBBtnGroup, MDBIcon } from 'mdb-react-ui-kit';
 
@@ -14,6 +15,24 @@ export default function Home() {
       margin: '0 auto',
     },
   };
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [smallScreen, setSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenWidth < 800) {
+      setSmallScreen(true);
+    } else {
+      setSmallScreen(false);
+    }
+  }, [screenWidth]);
+
   return (
     <>
       <header style={{ paddingLeft: 0 }}>
@@ -21,16 +40,16 @@ export default function Home() {
           className='p-5 text-center bg-image'
           style={styles.bg}
         >
-          <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+          <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
             <div className='d-flex flex-column justify-content-center align-items-center h-100'>
               <div className='text-white hero'>
                 <h1 className='mb-5 hero-title'>Hi, my name is Warren</h1>
                 <h4 className='mb-auto hero-sub'>
-                  I AM A COMPUTING SCIENCE MAJOR AND BUSINESS MINOR<br />
+                  I AM A COMPUTING SCIENCE MAJOR + BUSINESS MINOR<br />
                   STUDENT AT THE UNIVERSITY OF ALBERTA.
                 </h4>
               </div>
-              <MDBBtnGroup size='lg' aria-label='Navigation buttons' className='mt-5'>
+              <MDBBtnGroup size='lg' aria-label='Navigation buttons' className='mt-5' vertical={smallScreen}>
                 <MDBBtn color='light' outline>
                   <div className='hero-btn'>
                   <MDBIcon size='lg' fas icon='pen-square' /> &nbsp;
