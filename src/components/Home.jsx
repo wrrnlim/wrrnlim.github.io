afff86e6dd4a576338ae05e660af489cdda4df73
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2022-12-21 00:28:03 -0700

    complete landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,65 @@
 import React from 'react';
-import '../css/home.css';
+import '../css/home.css'
+import { MDBBtn, MDBBtnGroup, MDBIcon } from 'mdb-react-ui-kit';
 
 export default function Home() {
- return (
-	<div id="header">
-    <h1>Hi, my name is Warren</h1>
-      <p>I AM A COMPUTING SCIENCE MAJOR AND BUSINESS MINOR <br />
-          STUDENT AT THE UNIVERSITY OF ALBERTA.</p>
-		<nav>
-			<ul>
-				<li><a href="#intro">Intro</a></li>
-				<li><a href="#work">Work</a></li>
-				<li><a href="#about">About</a></li>
-				<li><a href="#contact">Contact</a></li>
-			
-			</ul>
-		</nav>
-	</div>
-    )
+  const styles = {
+    bg: {
+      backgroundImage: `url('../images/bg.png')`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      width: '100vw',
+      height: '100vh',
+      margin: '0 auto',
+    },
+  };
+  return (
+    <>
+      <header style={{ paddingLeft: 0 }}>
+        <div
+          className='p-5 text-center bg-image'
+          style={styles.bg}
+        >
+          <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+            <div className='d-flex flex-column justify-content-center align-items-center h-100'>
+              <div className='text-white hero'>
+                <h1 className='mb-5 hero-title'>Hi, my name is Warren</h1>
+                <h4 className='mb-auto hero-sub'>
+                  I AM A COMPUTING SCIENCE MAJOR AND BUSINESS MINOR<br />
+                  STUDENT AT THE UNIVERSITY OF ALBERTA.
+                </h4>
+              </div>
+              <MDBBtnGroup size='lg' aria-label='Navigation buttons' className='mt-5'>
+                <MDBBtn color='light' outline>
+                  <div className='hero-btn'>
+                  <MDBIcon size='lg' fas icon='pen-square' /> &nbsp;
+                  Blog
+                  </div>
+                </MDBBtn>
+                <MDBBtn color='light' outline>
+                  <div className='hero-btn'>
+                  <MDBIcon size='lg' fas icon='tools' /> &nbsp;
+                  Projects
+                  </div>
+                </MDBBtn>
+                <MDBBtn color='light' outline>
+                  <div className='hero-btn'>
+                    <MDBIcon size='lg' fab icon='github' /> &nbsp;
+                    GitHub
+                  </div>
+                </MDBBtn>
+                <MDBBtn color='light' outline>
+                  <div className='hero-btn'>
+                    <MDBIcon size='lg' fab icon='linkedin-in' /> &nbsp;
+                    LinkedIn
+                  </div>
+                </MDBBtn>
+              </MDBBtnGroup>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
