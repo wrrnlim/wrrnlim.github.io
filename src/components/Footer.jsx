1c18e66abfe9adb8dbaf85d9f46ca446ac6df2e5
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2022-12-30 15:16:44 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { MDBFooter } from 'mdb-react-ui-kit';
 export default function Footer() {
   return (
     <MDBFooter className='text-center text-lg-left text-light'>
-      <div className='text-center mt-5 footer-text'>
+      <div className='text-center m-5 footer-text'>
         &copy; Warren Lim {new Date().getFullYear()} 
       </div>
     </MDBFooter>
