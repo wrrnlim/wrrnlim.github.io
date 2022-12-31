3745b0c243501717192439e65ca343ff1f5f96db
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2022-12-31 12:54:54 -0700

    Center blog page text

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -5,13 +5,15 @@ import Background from '../components/Background'
 export default function Blog() {
   return (
     <Background>
-      <h1 className='mb-5 hero-title text-light'>Blog page coming soon!</h1>
-      <MDBBtn color='light' outline href='/' className="mt-5">
-        <div className='hero-btn'>
-          <MDBIcon size='md' fas icon='house' /> &nbsp;
-          Home
-        </div>
-      </MDBBtn>
+      <div className='d-flex flex-column justify-content-center align-items-center h-100'>
+        <h1 className='mb-5 hero-title text-light'>Blog page coming soon!</h1>
+        <MDBBtn color='light' outline href='/' className="mt-5">
+          <div className='hero-btn'>
+            <MDBIcon size='md' fas icon='house' /> &nbsp;
+            Home
+          </div>
+        </MDBBtn>
+      </div>
     </Background>
   )
 }
