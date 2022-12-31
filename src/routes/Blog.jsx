46a3637f178222ad254cacf77a19e2a85f947075
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2022-12-31 12:55:32 -0700

    Add navbar to blog page

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -1,10 +1,12 @@
 import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit'
 import React from 'react'
 import Background from '../components/Background'
+import Navbar from '../components/Navbar'
 
 export default function Blog() {
   return (
     <Background>
+    <Navbar/>
       <div className='d-flex flex-column justify-content-center align-items-center h-100'>
         <h1 className='mb-5 hero-title text-light'>Blog page coming soon!</h1>
         <MDBBtn color='light' outline href='/' className="mt-5">
