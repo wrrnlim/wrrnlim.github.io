6157b4998b331705fc6d128c76bedf0e6df0229b
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2025-04-02 17:41:28 -0600

    Add about section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Card from './Card'
+import { FaArrowRight, FaLinkedin } from 'react-icons/fa'
+import profileImg from '../assets/profile_img.jpeg'
 
 export default function Section(props) {
   const { title } = props
@@ -12,33 +14,37 @@ export default function Section(props) {
   return (
     <div className="text-center">
       <h3 className='text-3xl font-bold my-5' id={title.toLowerCase()}>{title}</h3>
-      <div className="rounded-xl p-8 border-gray-200 border max-w-4xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4">Education</h3>
-            <ul className="list-disc list-inside text-left">
-              <li>
-                University of Alberta - Bachelor of Science in Computer Science with Business Minor (2019-2023)
-              </li>
-              <li>
-                International Baccalaureate Diploma (2016-2019)
-              </li>
-            </ul>
+      <div className="rounded-xl p-8 border-gray-200 border shadow-sm hover:shadow-md transition-shadow max-w-4xl mx-5 lg:mx-auto mb-10">
+        <div className="grid grid-cols-1 md:grid-cols-10 gap-6">
+          <div className="md:col-span-3 p-4 flex justify-center items-center">
+            <img
+              alt="Profile Photo"
+              src={profileImg}
+              className="max-h-48 w-auto rounded-full shadow-sm"
+            />
           </div>
+          <div className="md:col-span-7 p-4 flex flex-col justify-between text-left space-y-4">
+            <div className="space-y-4">
+              <div>
+                <h4 className="font-bold text-lg text-[#242935]">Education</h4>
+                <p className="ml-1">University of Alberta - BSc Computing Science with Business Minor (2019-2023)</p>
+              </div>
 
-          <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4"> Backend</h3>
-            <div className="flex flex-wrap gap-2">
-              {backendSkills.map((tech, key) => (
-                <span
-                  key={key}
-                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
-                    "
-                >
-                  {tech}
-                </span>
-              ))}
+              <div>
+                <h4 className="font-bold text-lg text-[#242935]">Latest Work Experience</h4>
+                <p className="ml-1">Software Developer at Trust Science (2021-2025)</p>
+              </div>
+            </div>
+
+            <div className="pt-4 border-t border-gray-100">
+              <p className="text-gray-600 italic text-sm">View all my education, work experience, and updated information on my LinkedIn profile.</p>
+              <div className="mt-3 self-end flex justify-end">
+                <a href="https://linkedin.com/in/yourprofile" className="text-[#242935] hover:text-[#facb72] font-medium hover:cursor-pointer flex items-center gap-2 transition-colors duration-200 group">
+                  <FaLinkedin className="text-lg" />
+                  <span className="underline">View on LinkedIn</span>
+                  <FaArrowRight size={12} className="group-hover:translate-x-1 transition-transform duration-200" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
