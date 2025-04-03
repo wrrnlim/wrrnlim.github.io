54457a9889eef84dff066c1034ec4096c2305acc
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2025-04-02 18:04:08 -0600

    Update subtext colours

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -23,17 +23,17 @@ export default function Section(props) {
               <div>
                 <h4 className="font-bold text-xl text-gray-800">Education</h4>
                 <p className="text-gray-700">University of Alberta - BSc Computing Science with Business Minor</p>
-                <p className="text-gray-400">Sep 2019 - Apr 2023</p>
+                <p className="text-gray-500">Sep 2019 - Apr 2023</p>
               </div>
               <div>
                 <h4 className="font-bold text-xl text-gray-800">Recent Work Experience</h4>
                 <p className="text-gray-700">Software Developer at Trust Science</p>
-                <p className="text-gray-400">Sep 2021 - Mar 2025</p>
+                <p className="text-gray-500">Sep 2021 - Mar 2025</p>
               </div>
             </div>
 
             <div className="pt-4 border-t border-gray-100">
-              <p className="text-gray-600 italic text-sm">View all my education, work experience, and updated information on my LinkedIn profile.</p>
+              <p className="text-gray-500 italic text-sm">View all my education, work experience, and updated information on my LinkedIn profile.</p>
               <div className="mt-3 self-end flex justify-end">
                 <a href={socialLinks.linkedin} target='_blank' className="text-gray-800 hover:bg-[#ffcd71]/20 px-3 py-1 rounded-lg font-medium flex items-center gap-2 transition-all duration-200 group">
                   <FaLinkedin className="text-lg" />
